6769a506f645df85c9373206212963fb0bdcc950
lzt <1134565243@qq.com>
2022-08-19 08:45:39 +0800

    aaa

--- packages/vue2-eslint/lib/vue2-eslint.js
+++ packages/vue2-eslint/lib/vue2-eslint.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: ["eslint-config-basic-eslint"],
     rules: {
         // 算引号和分号结尾
+        "quotes": ["error", "double"],
         "semi": ["error", "always"],
     }
 };
