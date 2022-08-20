a9a0d955586fa6d9599c71cce15489534fc58db8
lzt <1134565243@qq.com>
2022-08-20 19:45:04 +0800

    feat: 完成ts规则的编写

--- packages/eslint-config-reacthooks-eslint/lib/index.js
+++ packages/eslint-config-reacthooks-eslint/lib/index.js
@@ -1,11 +1,8 @@
 module.exports = {
-    extends: ["eslint-config-basic-eslint", 'plugin:@typescript-eslint/recommended'],
+    extends: ['./rules/ts',],
     // 解析TS
     parser: '@typescript-eslint/parser',
     // 这允许您在代码库中使用typescript-eslint的规则
     plugins: ['@typescript-eslint'],
-    rules: {
-        // 
-        "@typescript-eslint/adjacent-overload-signatures": "off"
-    }
+    rules: {}
 };
---
+++ packages/eslint-config-reacthooks-eslint/lib/rules/ts.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["eslint-config-basic-eslint", 'plugin:@typescript-eslint/recommended'],
+  // 解析TS
+  parser: '@typescript-eslint/parser',
+  // 这允许您在代码库中使用typescript-eslint的规则
+  plugins: ['@typescript-eslint'],
+  rules: {
+    "@typescript-eslint/adjacent-overload-signatures": "off",
+    "@typescript-eslint/await-thenable": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    '@typescript-eslint/ban-types': 'off',
+    "@typescript-eslint/no-floating-promises": "off",
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  }
+};
