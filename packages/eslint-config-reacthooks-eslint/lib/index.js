03dc086a14b3355a6cec03c1dd4805beb9e7df41
lzt <1134565243@qq.com>
2022-08-21 20:32:00 +0800

    feat: 完成react规则的制定

--- packages/eslint-config-reacthooks-eslint/lib/index.js
+++ packages/eslint-config-reacthooks-eslint/lib/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['./rules/ts',],
+    extends: ['./rules/ts','./rules/react'],
     // 解析TS
     parser: '@typescript-eslint/parser',
     // 这允许您在代码库中使用typescript-eslint的规则
---
+++ packages/eslint-config-reacthooks-eslint/lib/rules/react.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jasmine: true,
+    jest: true,
+    es6: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2021,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+
+  plugins: [
+    "react",
+    //专门用来检查 Hooks 是否正确被使用
+    "react-hooks",
+  ],
+  extends: [
+    "plugin:react/recommended",
+    "eslint-config-basic-eslint"
+  ],
+  settings: {
+    react: {
+      //从` package.json '中检测当前的 React 版本
+      version: "detect"
+    }
+  },
+  globals: {
+    React: true,
+    ReactDOM: true,
+  },
+  rules: {
+    // 检查 Hooks 的使用规则
+    "react-hooks/rules-of-hooks": "error",
+    'react/prop-types': "off",
+    'react/display-name': "off",
+  }
+};
